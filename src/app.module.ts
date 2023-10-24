5195b210a7c8fc32afa5086e072b5c42931cd9ec
Marshall Ku <marshall@kakao.com>
2023-10-24 21:30:24 +0900

    Fix formatting of file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,21 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import Joi from 'joi';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import Joi from "joi";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      envFilePath: `.env`,
-      validationSchema: Joi.object({
-        NODE_PORT: Joi.string().required(),
-        REDIS_PORT: Joi.string().required(),
-        REDIS_HOST: Joi.string().required(),
-      }),
-    }),
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: `.env`,
+            validationSchema: Joi.object({
+                NODE_PORT: Joi.string().required(),
+                REDIS_PORT: Joi.string().required(),
+                REDIS_HOST: Joi.string().required(),
+            }),
+        }),
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
