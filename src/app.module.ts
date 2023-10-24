de2e7e19b0da8a9e9a8230d68b6c30c1b59bf4c8
Marshall Ku <marshall@kakao.com>
2023-10-24 21:22:15 +0900

    Add environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import Joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env`,
+      validationSchema: Joi.object({
+        NODE_PORT: Joi.string().required(),
+        REDIS_PORT: Joi.string().required(),
+        REDIS_HOST: Joi.string().required(),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
