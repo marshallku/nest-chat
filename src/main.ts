0b9e4d8dff2bb6d3e345dbcc888c247fbbb80fae
Marshall Ku <marshall@kakao.com>
2023-10-25 22:08:44 +0900

    Add redis io adapter

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { RedisIoAdapter } from "#chat/chat.adapter";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    const redisIOAdapter = new RedisIoAdapter(app);
+    await redisIOAdapter.connectToRedis();
+
+    app.useWebSocketAdapter(redisIOAdapter);
+
     await app.listen(process.env.NODE_PORT);
 }
 bootstrap();
