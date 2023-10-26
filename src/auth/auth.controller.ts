cd7049468d63a85e6d2d73764e9250f09a7827c5
Marshall Ku <marshall@kakao.com>
2023-10-26 21:19:51 +0900

    Add auth guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,21 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from "@nestjs/common";
+import { Body, Controller, Get, HttpCode, HttpStatus, Post, Request } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { User } from "#users/users.service";
+import { Public } from "./auth.guard";
 
 @Controller("auth")
 export class AuthController {
     constructor(private authService: AuthService) {}
 
+    @Public()
     @HttpCode(HttpStatus.OK)
     @Post("login")
     signIn(@Body() signInDto: Pick<User, "name" | "password">) {
         return this.authService.signIn(signInDto.name, signInDto.password);
+    }
+
+    @Get("profile")
+    getProfile(@Request() req: { user: User }) {
+        return req.user;
     }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,11 @@
+import { APP_GUARD } from "@nestjs/core";
 import { Module } from "@nestjs/common";
-import { UsersModule } from "#users/users.module";
+import { ConfigModule } from "@nestjs/config";
+import { JwtModule } from "@nestjs/jwt";
+import { AuthGuard } from "./auth.guard";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
-import { JwtModule } from "@nestjs/jwt";
-import { ConfigModule } from "@nestjs/config";
+import { UsersModule } from "#users/users.module";
 
 @Module({
     imports: [
@@ -21,7 +23,13 @@ import { ConfigModule } from "@nestjs/config";
         }),
     ],
     controllers: [AuthController],
-    providers: [AuthService],
+    providers: [
+        AuthService,
+        {
+            provide: APP_GUARD,
+            useClass: AuthGuard,
+        },
+    ],
     exports: [AuthService],
 })
 export class AuthModule {}
