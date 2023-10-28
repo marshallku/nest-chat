3bc472872b4f1700ff99672d0e6299e64c75da86
Marshall Ku <marshall@kakao.com>
2023-10-28 23:43:35 +0900

    Add schema for chat room

--- src/room/room.module.ts
+++ src/room/room.module.ts
@@ -1,8 +1,11 @@
 import { Module } from "@nestjs/common";
 import { RoomService } from "./room.service";
 import { RoomController } from "./room.controller";
+import { MongooseModule } from "@nestjs/mongoose";
+import { Room, RoomSchema } from "./room.schema";
 
 @Module({
+    imports: [MongooseModule.forFeature([{ name: Room.name, schema: RoomSchema }])],
     providers: [RoomService],
     controllers: [RoomController],
 })
---
+++ src/room/room.schema.ts
@@ -0,0 +1,29 @@
+import { HydratedDocument, Types } from "mongoose";
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { ChatUserRole } from "#constants/lib/user";
+
+export type RoomUserDocument = HydratedDocument<RoomUser>;
+
+@Schema()
+export class RoomUser {
+    @Prop({ type: Types.ObjectId, ref: "User", required: true })
+    user: Types.ObjectId;
+
+    @Prop({ required: true })
+    role: ChatUserRole;
+}
+
+export const RoomUserSchema = SchemaFactory.createForClass(RoomUser);
+
+export type RoomDocument = HydratedDocument<Room>;
+
+@Schema({ timestamps: true })
+export class Room {
+    @Prop({ required: true })
+    name: string;
+
+    @Prop({ type: [RoomUserSchema], required: true })
+    users: RoomUser[];
+}
+
+export const RoomSchema = SchemaFactory.createForClass(Room);
