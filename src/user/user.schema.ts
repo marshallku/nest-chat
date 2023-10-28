a1f89fa97b2490643b9bd9bb52fc7b972eae081f
Marshall Ku <marshall@kakao.com>
2023-10-28 20:46:17 +0900

    Modify User class to extend Document from mongoose

--- src/user/user.schema.ts
+++ src/user/user.schema.ts
@@ -1,13 +1,11 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
-import { HydratedDocument } from "mongoose";
+import { Document, HydratedDocument } from "mongoose";
 import { UserRole } from "#constants/lib/user";
 
 export type UserDocument = HydratedDocument<User>;
 
 @Schema({ timestamps: true })
-export class User {
-    _id: string;
-
+export class User extends Document<string> {
     @Prop({ required: true })
     name: string;
 
