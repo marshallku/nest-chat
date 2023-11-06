a43391bbb6d6137a334f722c33713823f2989db6
Marshall Ku <marshall@kakao.com>
2023-11-06 22:22:47 +0900

    Allow cross origin requests

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ import { RedisIoAdapter } from "#chat/chat.adapter";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule, {
+        cors: {
+            credentials: true,
+            origin: ["http://localhost:3000", "https://chat.marshallku.dev", "https://marshallku.dev:48030"],
+        },
+    });
 
     const config = new DocumentBuilder().setTitle("Chat api").setVersion("1.0").build();
     const document = SwaggerModule.createDocument(app, config);
